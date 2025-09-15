f99d3495b1654c84c5852a74b99afcc3933dd9fd
Lcdz85 <lo.coudyzer@gmail.com>
2025-09-15 20:28:16 +0200

    add CarnetFixtures

---
+++ src/DataFixtures/CarnetFixtures.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Carnet;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Symfony\Component\Validator\Constraints\Length;
+
+class CarnetFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {   
+        $faker = Factory::create('fr_BE');
+        $modeleTitres = ["Sur la route de ",
+                        "Souvenirs de ",
+                        "Escapade en ",
+                        "Chroniques de ",
+                        "À la découverte de ",];
+        
+        for ($i=0; $i<10; $i++)
+        {
+            $lieu = $faker->boolean() ? $faker->unique()->city() : $faker->unique()->country();
+            $photo = "https://picsum.photos/800/600?random=" . rand(1, 200);
+
+            $carnet = new Carnet();
+            $carnet->setTitre($modeleTitres[rand(0,count($modeleTitres)-1)] . $lieu)
+                   ->setDateCarnet($faker->dateTimeBetween('-2 year', 'now'))
+                   ->setPhoto($photo);
+
+            $manager->persist($carnet);
+        }
+
+        $manager->flush();
+    }
+}
