3a40bce47bd6d22fe69a48687f2b129aa7b2f94a
Lcdz85 <lo.coudyzer@gmail.com>
2025-12-01 19:31:53 +0100

    fixture localisation posts de carnet dans un meme rayon

--- src/DataFixtures/PostFixtures.php
+++ src/DataFixtures/PostFixtures.php
@@ -19,20 +19,52 @@ class PostFixtures extends Fixture implements DependentFixtureInterface
         $carnets = $manager->getRepository(Carnet::class)->findAll();
         $photo = "https://picsum.photos/800/600?random=" . rand(200, 500);
         
+        // Créer un tableau pour stocker les coordonnées de référence par carnet
+        $carnetCoordinates = [];
+        
+        // Définir une position de référence pour chaque carnet
+        foreach ($carnets as $carnet) {
+            $carnetCoordinates[$carnet->getId()] = [
+                'latitude' => $faker->latitude(),
+                'longitude' => $faker->longitude()
+            ];
+        }
 
-        for ($i=1; $i<=30; $i++)
-        {
+        // Rayon maximum en degrés (approximatif pour 250 km)
+        $maxRadiusDeg = 2.5;
+
+        for ($i=1; $i<=30; $i++) {
             $post = new Post();
+            
+            // Associer un Carnet existant (créé par CarnetFixtures)
+            $carnet = $carnets[array_rand($carnets)];
+            $carnetId = $carnet->getId();
+            
+            // Obtenir les coordonnées de référence pour ce carnet
+            $baseLat = $carnetCoordinates[$carnetId]['latitude'];
+            $baseLng = $carnetCoordinates[$carnetId]['longitude'];
+            
+            // Générer un décalage aléatoire en degrés
+            $offsetLat = $faker->randomFloat(6, -$maxRadiusDeg, $maxRadiusDeg);
+            $offsetLng = $faker->randomFloat(6, -$maxRadiusDeg, $maxRadiusDeg);
+            
+            // Ajuster la longitude en fonction de la latitude pour une meilleure répartition
+            $offsetLng = $offsetLng / cos(deg2rad($baseLat));
+            
+            // Calculer les nouvelles coordonnées
+            $newLat = $baseLat + $offsetLat;
+            $newLng = $baseLng + $offsetLng;
+            
+            // S'assurer que les coordonnées sont valides
+            $newLat = max(-90, min(90, $newLat));
+            $newLng = fmod(($newLng + 540) % 360 - 180, 360);
 
             $post->setTitre($faker->sentence(rand(3, 6)))
                  ->setDatePost($faker->dateTimeBetween('-2 year', 'now'))
                  ->setTexte($faker->text(rand(50,200)))
-                 ->setLatitude($faker->latitude())
-                 ->setLongitude($faker->longitude());
-
-            // Associer un Carnet existant (créé par CarnetFixtures)
-            $carnet = $carnets[array_rand($carnets)];
-            $post->setCarnet($carnet);
+                 ->setLatitude($newLat)
+                 ->setLongitude($newLng)
+                 ->setCarnet($carnet);
 
             $nbPhotos = rand(1, 4); // entre 1 et 4 photos par post
             for ($j = 0; $j < $nbPhotos; $j++) {
