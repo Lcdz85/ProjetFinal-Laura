bfcf68e868ad3b23a1c2776ecd073115805acd83
Lcdz85 <lo.coudyzer@gmail.com>
2025-12-01 22:11:28 +0100

    modif style js des reponse commentaire et bouton publier et envoyer

--- migrations/Version20251201210505.php
+++ migrations/Version20251201210505.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20251201175300 extends AbstractMigration
+final class Version20251201210505 extends AbstractMigration
 {
     public function getDescription(): string
     {
