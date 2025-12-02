b82e731a054ce5b1489c90cb49aeadd57ae1132f
Laura <l.coudyzer@interface3.be>
2025-12-02 15:14:47 +0100

    routelogin

---
+++ databaseRestart.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Ce script va supprimer la base de données et la recréer."
+symfony console doctrine:database:drop --force
+symfony console doctrine:database:create
+
+rm migrations/Ve*
+symfony console make:migration --no-interaction
+symfony console doctrine:migrations:migrate --no-interaction
+symfony console doctrine:fixtures:load --no-interaction
